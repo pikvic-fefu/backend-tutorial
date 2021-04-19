445d9d367d0cc025ed85cb1738723e928fd83a0c
pikvic <truepikvic@gmail.com>
2021-04-19 21:11:05 +1000

    Add post to database. Works.

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ POST_QUERY = """SELECT * FROM posts WHERE id = ?"""
 INSERT_QUERY = """INSERT INTO posts (title, text, author, date)
                 VALUES ('Hello', 'World 123', 'Vasya', '2002-01-02')
                 """
+INSERT_POST_QUERY = """INSERT INTO posts (title, text, author, date)
+                VALUES (?, ?, ?, ?)
+                """
+
 
 def get_db_connection():
     conn = sqlite3.connect(DATABASE)
@@ -22,36 +26,35 @@ def get_db_connection():
     return conn
 
 def create_db():
-    conn = get_db_connection()
-    conn.execute(CREATE_QUERY)
-    conn.commit()
-    conn.close()
+    with get_db_connection() as conn:
+        conn.execute(CREATE_QUERY)
 
 def init_db():
     if not os.path.exists(DATABASE):
         create_db()
-        conn = get_db_connection()
-        conn.execute(INSERT_QUERY)
-        conn.commit()
-        conn.close()
+        with get_db_connection() as conn:
+            conn.execute(INSERT_QUERY)
+            conn.commit()
 
 def get_posts():
-    conn = get_db_connection()
-    posts = conn.execute(POSTS_QUERY).fetchall()
-    conn.close()
+    with get_db_connection() as conn:
+        posts = conn.execute(POSTS_QUERY).fetchall()
     return posts
 
 def get_post(id):
-    conn = get_db_connection()
-    post = conn.execute(POST_QUERY, [(id)]).fetchone()
-    conn.close()
+    with get_db_connection() as conn:
+        post = conn.execute(POST_QUERY, [id]).fetchone()
     return post
+
+def insert_post(post):
+    with get_db_connection() as conn:
+        parameters = [post['title'], post['text'], post['author'], post['date']]
+        conn.execute(INSERT_POST_QUERY, parameters)
+        conn.commit()
 
 app = Flask(__name__)
 
 init_db()
-
-posts = get_posts()
 
 @app.route('/')
 def index():
@@ -59,9 +62,8 @@ def index():
 
 @app.route('/posts/<int:id>')
 def about(id):
-    if id not in range(1, len(posts) + 1):
-        return "ERROR! Id is out of range"
-    print(dict(get_post(id)))
+    if id not in range(1, len(get_posts()) + 1):
+        return "", 404
     return render_template('post.html', post=get_post(id))
 
 @app.route('/posts/add')
@@ -70,16 +72,12 @@ def posts_add():
 
 @app.route('/addpost', methods=['post'])
 def addpost():
-    global posts
     if request.form:
-        title = request.form.get('title')  # запрос к данным формы
+        title = request.form.get('title')
         author = request.form.get('author')
         date = request.form.get('date')
         text = request.form.get('text')
         tags = request.form.get('tags')
-        id = len(posts)
-        post = Post(id, title, author, date, text, tags)
-        print(post)
-        post.save_post(DATABASE)
-        posts = read_posts_from_file(DATABASE)
+        post = {'title': title, 'author': author, 'date': date, 'text': text}
+        insert_post(post)
     return redirect('/')
