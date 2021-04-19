6507aee3ca3c940dd216d99e1685135ed3c22f6d
pikvic <truepikvic@gmail.com>
2021-04-19 18:54:23 +1000

    Add database. Not full support

--- app.py
+++ app.py
@@ -1,23 +1,68 @@
 from flask import Flask, render_template, redirect, request
 from datetime import datetime
 from post.post import read_posts_from_file, Post
+import sqlite3
+import os
 
-DATABASE = 'posts.txt'
+
+DATABASE = 'posts.db'
+CREATE_QUERY = """CREATE TABLE IF NOT EXISTS posts
+                  (id integer primary key, title text, text text,
+                   author text, date text)
+               """
+POSTS_QUERY = """SELECT * FROM posts"""
+POST_QUERY = """SELECT * FROM posts WHERE id = ?"""
+INSERT_QUERY = """INSERT INTO posts (title, text, author, date)
+                VALUES ('Hello', 'World 123', 'Vasya', '2002-01-02')
+                """
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    conn = get_db_connection()
+    conn.execute(CREATE_QUERY)
+    conn.commit()
+    conn.close()
+
+def init_db():
+    if not os.path.exists(DATABASE):
+        create_db()
+        conn = get_db_connection()
+        conn.execute(INSERT_QUERY)
+        conn.commit()
+        conn.close()
+
+def get_posts():
+    conn = get_db_connection()
+    posts = conn.execute(POSTS_QUERY).fetchall()
+    conn.close()
+    return posts
+
+def get_post(id):
+    conn = get_db_connection()
+    post = conn.execute(POST_QUERY, [(id)]).fetchone()
+    conn.close()
+    return post
 
 app = Flask(__name__)
 
-posts = read_posts_from_file(DATABASE)
+init_db()
+
+posts = get_posts()
 
 @app.route('/')
 def index():
-    print(posts)
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=get_posts())
 
 @app.route('/posts/<int:id>')
 def about(id):
-    if id not in range(len(posts)):
-        return "ERROR! Id is out of range" 
-    return render_template('post.html', post=posts[id])
+    if id not in range(1, len(posts) + 1):
+        return "ERROR! Id is out of range"
+    print(dict(get_post(id)))
+    return render_template('post.html', post=get_post(id))
 
 @app.route('/posts/add')
 def posts_add():
